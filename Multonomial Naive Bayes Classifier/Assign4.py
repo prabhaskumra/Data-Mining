cf8439769d4212a167fdebe56a2f826b5d0574b0
prabhaskumra <prabhas1998@gmail.com>
2020-02-23 18:54:42 -0800

    Create Assign4.py

---
+++ Multonomial Naive Bayes Classifier/Assign4.py
@@ -0,0 +1,31 @@
+# Prabhas Kumra
+# CS-458
+# Assignment-4
+
+import nltk
+from nltk.corpus import movie_reviews
+nltk.download('movie_reviews')
+
+from sklearn.naive_bayes import MultinomialNB
+
+
+#1
+def build_raw_data():
+    print("build_raw_data")
+
+#2
+def feature_selection():
+    print(" beep bop")
+
+#3
+def text_to_vector():
+    print("beep baap")
+
+#4
+def split_data():
+    print("hahaahahah")
+
+
+#5 
+def model():
+    print("model")
