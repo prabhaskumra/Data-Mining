1e47018f023b040eae400f5dbf8e26db29dd42b2
prabhaskumra <prabhas1998@gmail.com>
2020-03-07 16:14:50 -0800

    dropped 'hp' column from testing and training set

--- Logistic Regression/Assign5.py
+++ Logistic Regression/Assign5.py
@@ -2,3 +2,21 @@
 # Assignment-5
 # CS-458
 
+import pandas as pd 
+
+training_set = pd.read_csv('train.csv')
+testing_set = pd.read_csv('test.csv')
+
+print(training_set)
+print(" ")
+print(testing_set)
+
+
+del training_set['hp']
+del testing_set['hp']
+
+print(training_set)
+print(" ")
+print(testing_set)
+
+
