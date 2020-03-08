46c89a4397973fef4bbdb2d084a0190060bba2fe
prabhaskumra <prabhas1998@gmail.com>
2020-03-08 14:58:15 -0700

    Update Assign5.py

--- Logistic Regression/Assign5.py
+++ Logistic Regression/Assign5.py
@@ -4,19 +4,26 @@
 
 import pandas as pd 
 
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix
+
 training_set = pd.read_csv('train.csv')
 testing_set = pd.read_csv('test.csv')
 
-print(training_set)
-print(" ")
-print(testing_set)
+# print(training_set)
+# print(" ")
+# print(testing_set)
 
 
+# training_set.drop('hp',implace=True)
+# testing_set.drop('hp',implace=True)
 del training_set['hp']
 del testing_set['hp']
 
-print(training_set)
-print(" ")
-print(testing_set)
+# print(training_set)
+# print(" ")
+# print(testing_set)
+
+
 
 
