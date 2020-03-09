deef6f43ef3d0267cd9f49fdefd49c60627379ee
prabhaskumra <prabhas1998@gmail.com>
2020-03-08 18:05:17 -0700

    X,Y

--- Logistic Regression/Assign5.py
+++ Logistic Regression/Assign5.py
@@ -5,25 +5,56 @@
 import pandas as pd 
 
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import confusion_matrix
 
-training_set = pd.read_csv('train.csv')
-testing_set = pd.read_csv('test.csv')
+from sklearn.model_selection import train_test_split
+
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+
+training_set = pd.read_csv('train.csv', header=0)
+testing_set = pd.read_csv('test.csv', header=0)
 
 # print(training_set)
 # print(" ")
 # print(testing_set)
 
+df = pd.read_csv('mtcars.csv', header=0)
 
 # training_set.drop('hp',implace=True)
 # testing_set.drop('hp',implace=True)
-del training_set['hp']
-del testing_set['hp']
 
 # print(training_set)
 # print(" ")
 # print(testing_set)
 
+del training_set['hp']
+# training_set.drop('hp',  inplace=True)
+del testing_set['hp']
+# training_set.drop('hp', inplace=True)
 
 
+# print(training_set)
+# print(" ")
+# print(testing_set)
+# print(training_set)
+# print(training_set.head())
+X = training_set
+Y = training_set.am
+
+# print(X)
+# print(Y)
+
+# X_train, X_test, y_train, y_test = train_test_split(X,Y,train_size=0.3)
+
+# print(X_test)
+
+clf = LogisticRegression()
+clf.fit(X,Y)
+y_pred = clf.predict(testing_set)
+
+print(y_pred)
+
+
+# print("Accuracy: ", accuracy_score(y_test, y_pred))
+# print(classification_report(y_test, y_pred))
+# print(confusion_matrix(y_test, y_pred))
 
