302d7ec29a2d94a18938b44403ad8ff82a600214
prabhaskumra <prabhas1998@gmail.com>
2020-03-10 01:41:30 -0700

    Updated the learning model and input

--- Logistic Regression/Assign5.py
+++ Logistic Regression/Assign5.py
@@ -4,7 +4,6 @@
 
 import pandas as pd 
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 
 training_set = pd.read_csv('train.csv', header=0)
@@ -13,16 +12,15 @@ testing_set = pd.read_csv('test.csv', header=0)
 del training_set['hp']
 del testing_set['hp']
 
-X = training_set
+X = training_set[['mpg','cyl','disp','drat','wt','qsec','vs','gear','carb']]
 Y = training_set.am
-del X['car_model']
-del testing_set['car_model']
 
+testing = testing_set[['mpg','cyl','disp','drat','wt','qsec','vs','gear','carb']]
 y_true = testing_set.am
 
 clf = LogisticRegression(max_iter=1000)
 clf.fit(X,Y)
-y_pred = clf.predict(testing_set)
+y_pred = clf.predict(testing)
 
 print("Accuracy: ", accuracy_score(y_true, y_pred))
 print(classification_report(y_true, y_pred))
