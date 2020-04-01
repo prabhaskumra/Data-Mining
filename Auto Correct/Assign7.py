c3f098d6727af57ce3211529841dd2ae87805111
prabhaskumra <prabhas1998@gmail.com>
2020-03-31 18:35:08 -0700

    Added Autocorrect

---
+++ Auto Correct/Assign7.py
@@ -0,0 +1,24 @@
+# Prabhas Kumra
+# Assignment #7
+# CS-458
+
+filename = "Pride&Prejudice.txt"
+
+h = 0
+f = open("Pride&Prejudice.txt", "r")
+print(type(f))
+for x in f:
+    # print(x)
+    data += x.lower()
+    # print(x)
+    # h+=1
+    # if h == 5:
+    #     break
+
+# with open(filename) as f:
+#     data = f.readlines()
+#     data.lower()
+
+
+# print(data)
+# print(data)
