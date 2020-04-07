1b5ac30c4e75cb55e3355a867e88768d33546aa9
prabhaskumra <prabhas1998@gmail.com>
2020-04-06 19:42:50 -0700

    Update Assign7.py

--- Auto Correct/Assign7.py
+++ Auto Correct/Assign7.py
@@ -1,26 +1,72 @@
 # Prabhas Kumra
 # Assignment #7
 # CS-458
+
 import re
 import string
 import random
 
-filename = "Pride&Prejudice.txt"
-PUNCT=re.compile("[!#$%&()*+,-./:;<=>?@[\]^_`{|}~123456789']")
 
-# reading in the file and storing in an array
-with open(filename) as f:
-    data = f.readlines()
+def readFile():
+    filename = "Pride&Prejudice.txt"
 
-# converting letters to lowecase
-for x in range(len(data)):
-    data[x] = data[x].lower()
-    data[x] = re.sub(PUNCT, " ", data[x])
+    # reading in the file and storing in an array
+    with open(filename) as f:
+        data = f.readlines()
+
+    return data
+
+def removePunct(data):
+    PUNCT=re.compile("[!#$%&()*+,-./:;<=>?@[\]^_`{|}~123456789']")
+    
+    # converting letters to lowecase and removing Punctuation 
+    for x in range(len(data)):
+        data[x] = data[x].lower()
+        data[x] = re.sub(PUNCT, " ", data[x])
+
+    # for i in range(5):
+    #     print(data[i])
+
+def buildKeys():
+    KEYS = ['qwerstyuiopasdfghjklzxcvbnm']
+    keys = list(KEYS)
+
+    a = ['s','q','w']
+    b = ['v','n','g']
+    c = ['v','x','d']
+    d = []
+    e = []
+    f = []
+    g = []
+    h = []
+    i = []
+    j = []
+    k = []
+    l = []
+    m = []
+    n = []
+    o = []
+    p = []
+    q = []
+    r = []
+    s = []
+    t = []
+    u = []
+    v = []
+    w = []
+    x = []
+    y = []
+    z = []
 
 
-for i in range(5):
-    print(data[i])
 
-KEYS = ['qwerstyuiopasdfghjklzxcvbnm']
-keys = list(KEYS)
-print(keys)
+    print(a[2])
+    print(z)
+
+def main():
+    data = readFile()
+    removePunct(data)
+    buildKeys()
+
+if __name__ == "__main__":
+    main()
